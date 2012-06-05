72aa6a7ee75708da49035aada9fcb897a0f881bf
David Pilato <david@pilato.fr>
2012-06-05 22:55:37 +0200

    Remove old comments

--- src/test/java/fr/pilato/spring/elasticsearch/AbstractESTest.java
+++ src/test/java/fr/pilato/spring/elasticsearch/AbstractESTest.java
@@ -4,18 +4,6 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-/**
- * <ul>
- * A utiliser pour tester les DAO .<br>
- * <li>Permet de recuperer les declarations (fichiers xml) communes aux DAO en
- * test.<br>
- * <li>Les DAO de test doivent donc deriver de cette classe.<br>
- * </ul>
- * 
- * 
- * @author ROBET
- */
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
 		"classpath:es-context.xml"
