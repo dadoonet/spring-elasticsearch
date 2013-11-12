c377dcb4ebfe3332f58e028bc9b01bfd044f297c
Matt Friedman <mfriedma@afilias.info>
2013-11-12 14:28:22 -0500

    Don't throw exception when we read a file in classpath but return null Closes #34.

---
+++ src/test/java/fr/pilato/spring/elasticsearch/ElasticsearchAbstractClientFactoryBeanTest.java
@@ -0,0 +1,25 @@
+package fr.pilato.spring.elasticsearch;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ *
+ */
+public class ElasticsearchAbstractClientFactoryBeanTest {
+
+    @Test
+    public void testReadFileInClasspath_ExpectFileReadOk() throws Exception {
+        String url = "classpath-reader-test.txt";
+        String contents = ElasticsearchAbstractClientFactoryBean.readFileInClasspath(url);
+        assertTrue("Expect to find file on classpath and read contents.", contents.startsWith("This file is here for testing purposes"));
+    }
+
+    @Test
+    public void testReadFileInClasspath_ExpectFileNotFound_ReturnsNull() throws Exception {
+        String url = "__unknown_file_path_____";
+        String contents = ElasticsearchAbstractClientFactoryBean.readFileInClasspath(url);
+        assertTrue("Expect file not found, return null.", contents == null);
+    }
+}
