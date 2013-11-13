a7b9c0b6acc27d6918d435f19b6275ed7ac0cb6a
Matt Friedman <mfriedma@afilias.info>
2013-11-13 10:35:03 -0500

    remove mockito; don't test logging

--- src/test/java/fr/pilato/spring/elasticsearch/ElasticsearchAbstractClientFactoryBeanTest.java
+++ src/test/java/fr/pilato/spring/elasticsearch/ElasticsearchAbstractClientFactoryBeanTest.java
@@ -1,27 +1,13 @@
 package fr.pilato.spring.elasticsearch;
 
-import org.apache.commons.logging.Log;
-import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 /**
  *
  */
 public class ElasticsearchAbstractClientFactoryBeanTest {
-
-    Log mockLogger;
-
-    @Before
-    public void setup() {
-        mockLogger = mock(Log.class);
-        ElasticsearchAbstractClientFactoryBean.logger = mockLogger;
-    }
 
     @Test
     public void testReadFileInClasspath_ExpectFileReadOk() throws Exception {
@@ -34,7 +20,6 @@ public class ElasticsearchAbstractClientFactoryBeanTest {
     public void testReadFileInClasspath_ExpectFileNotFound_ReturnsNull() throws Exception {
         String url = "__unknown_file_path_____";
         String contents = ElasticsearchAbstractClientFactoryBean.readFileInClasspath(url);
-        verify(mockLogger).error(eq( "Failed to load file from url: " + url), any(Exception.class));
         assertTrue("Expect file not found, return null.", contents == null);
     }
 }
